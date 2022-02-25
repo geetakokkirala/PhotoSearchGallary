f85ac052bc323f9bde3b83fb1e596a06de1223ca
geetakokkirala <geetakpb@gmail.com>
2022-02-25 16:30:55 +0530

    Initial Commit

---
+++ PhotoSearchGallary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoSearchGallary
+//
+//  Created by MAC on 25/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
