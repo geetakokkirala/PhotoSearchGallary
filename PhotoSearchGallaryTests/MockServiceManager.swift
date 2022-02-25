0a173d7c5d996a1857b337c2e6cc9c44aa2f737f
geetakokkirala <geetakpb@gmail.com>
2022-02-25 17:35:44 +0530

    Added test cased, created Mock Network manager

---
+++ PhotoSearchGallaryTests/MockServiceManager.swift
@@ -0,0 +1,39 @@
+//
+//  MockServiceManager.swift
+//  PhotoSearchGallaryTests
+//
+//  Created by MAC on 25/02/22.
+//
+
+import Foundation
+@testable import PhotoSearchGallary
+import Combine
+
+class MockServiceManager: Servicable {
+    func doApiCall(apiRequest: Requestable) -> Future<[PhotoDetail], ServiceError> {
+
+        return Future { promise in
+            
+            let bundle = Bundle(for:MockServiceManager.self)
+            
+            guard let url = bundle.url(forResource:apiRequest.path, withExtension:"json"),
+                  let data = try? Data(contentsOf: url)
+
+            else {
+                promise(.failure(ServiceError.dataNotFound))
+          
+                return
+            }
+            
+            guard let decodedResponse = try? JSONDecoder().decode(PhotoSearchResonce.self, from: data) else {
+                return promise(.failure(ServiceError.parsingError))
+            }
+            
+           let phototsDetails = decodedResponse.photos.photo.map {
+               PhotoDetail( title: $0.title, url: "\(APIEndPoints.imagesBaseUrl)/\($0.server)/\($0.id)_\($0.secret)_w.jpg")
+            }
+        
+            return promise(.success(phototsDetails))
+        }
+    }
+}
--- PhotoSearchGallaryTests/PhotoSearchGallaryTests.swift
+++ PhotoSearchGallaryTests/PhotoSearchGallaryTests.swift
@@ -9,25 +9,44 @@ import XCTest
 @testable import PhotoSearchGallary
 
 class PhotoSearchGallaryTests: XCTestCase {
-
+    
+    var viewModel:PhotoSearchViewModel!
+    var serviceManager:MockServiceManager!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        serviceManager = MockServiceManager()
+        
+        viewModel = PhotoSearchViewModel(serviceManager: serviceManager)
     }
+
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
+    
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testSearchPhotos_success() {
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let request = Request(baseUrl: APIEndPoints.baseUrl, path:"", params: ["method":APIEndPoints.photoMethod, "text":"cat", "api_key": APIEndPoints.apiKey, "format" : "json", "nojsoncallback" : "1"])
+
+        viewModel.search(request: request)
+        
+        XCTAssertEqual(viewModel.photoDetailsCount, 100)
+
+        XCTAssertEqual(viewModel.photoDetails.first?.title, "cat rider")
+
     }
+    
+    func testSearchPhotos_failure() {
+
+        let request = Request(baseUrl: APIEndPoints.baseUrl, path:"failedResonce", params: ["method":APIEndPoints.photoMethod, "text":"cat", "api_key": APIEndPoints.apiKey, "format" : "json", "nojsoncallback" : "1"])
+
+        viewModel.search(request: request)
+
+        XCTAssertEqual(viewModel.photoDetailsCount, 0)
+
+    }
+    
 
 }
